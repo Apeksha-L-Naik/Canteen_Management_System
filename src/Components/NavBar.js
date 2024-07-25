0f0355b5ee2cab8f838ac882070067b08f1663cb
Apeksha L Naik <apekshanaik0904@gmail.com>
2024-07-25 09:51:29 +0530

    Updated navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,6 +5,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
 import { useNavigate } from 'react-router-dom';
+import '../Images/logo.png';
 
 export default function NavBar() {
         const navigate = useNavigate();
@@ -16,22 +17,31 @@ export default function NavBar() {
             // Perform any logout actions (e.g., clear session, etc.)
             // Redirect to the login form
             navigate('/');
-          };     
+          }; 
+          const handleOrderButton=()=>{
+            navigate('/MenuItem')
+          }  
+          const handleHome=()=>{
+            navigate('/home')
+          }  
   return (
     <>
       <Navbar bg='danger' variant='dark' fixed="top">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home" style={{ fontFamily: "Bebas Neue",fontWeight:'bolder',fontSize:'22px'}}>FOOD HAVEN</Navbar.Brand>
+          {/* <img src={logo} alt="Logo" style={{ height: '40px', marginLeft: '10px' }}/> */}
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav" className="justify-content-between">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#home" onClick={handleHome}>Home</Nav.Link>
               <NavDropdown title="Account" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1" onClick={handleAdminClick}>Admin</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2" onClick={handleLogout}>Logout</NavDropdown.Item>
               </NavDropdown>
             </Nav>
-            <Button variant="outline-primary" href="#my-order">My Order</Button>
+            <Button 
+            onClick={handleOrderButton}
+            variant="outline" href="#my-order" style={{fontFamily:'bold',fontWeight:'bolder',color:'white',border:'2px solid white'}}>My Order</Button>
           </Navbar.Collapse>
         </Container>
       </Navbar>
